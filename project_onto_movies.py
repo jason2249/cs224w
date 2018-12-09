c054e19778eacb37bf3f9a9e47a8446f7e1a44a4
Nolan Handali <nolanhandali@gmail.com>
2018-12-08 23:18:14 -0800

    made projection more efficient

--- project_onto_movies.py
+++ project_onto_movies.py
@@ -23,22 +23,28 @@ def project_graph(graph):
 			new_graph.add_node(node)
 	pairs_done = set()
 	print(len(new_graph.nodes))
+	count = 0
 	for node1 in new_graph:
-		print node1
+		if count % 10 == 0:
+			print count
 		for node2 in new_graph:
 			if (node1,node2) in pairs_done or node1 == node2:
 				continue
 			node1_ratings = []
 			node2_ratings = []
-			for user in graph.neighbors(node1):
-				if user in graph.neighbors(node2):
-					node1_ratings.append(graph.edges[user,node1]['rating'])
-					node2_ratings.append(graph.edges[user,node2]['rating'])
+			neighbors = graph.neighbors(node1).intersection(graph.neighbors(node2))
+			if count % 10 == 0:
+				print("number of neighbors in common is")
+				print(len(neighbors))
+			for user in neighbors:
+				node1_ratings.append(graph.edges[user,node1]['rating'])
+				node2_ratings.append(graph.edges[user,node2]['rating'])
 			#if they are strongly correlated
 			if node1_ratings == []:
 				continue
 			if scipy.stats.pearsonr(node1_ratings, node2_ratings) > .5:
 				new_graph.add_edge(node1,node2)
+		count += 1
 	print(len(new_graph.edges))
 
 	return new_graph
@@ -54,10 +60,12 @@ def test_graph():
 	graph.add_edge(1,7, rating = 5)
 	graph.add_edge(2,6, rating = 5)
 	graph.add_edge(2,7, rating = 4)
-	return graph	
+	return graph
+
 if __name__ == '__main__':
 	graph = nx.read_gpickle('netflix_tiny.gpickle')
 	new_graph = project_graph(graph)
+	nx.write_gpickle(new_graph, "projected_graph_tiny.gpickle")
 	#graph = test_graph()
 	#print(project_graph(graph).edges)
 
